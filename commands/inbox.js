f8e94a420e352602ad85a559cc51b3624bdba50e
BOTX666 <botx6666@gmail.com>
2025-11-19 21:50:30 +0600

    Update inbox.js

--- commands/inbox.js
+++ commands/inbox.js
@@ -1,18 +1,31 @@
-const chatgpt = require('./commands/chatgpt');
-const gemini = require('./commands/gemini');
-const queenchat = require('./commands/queenchat');
+const help = require('./commands/help');
+const { getPrefix, isCommand } = require('./utils/prefix');
 
-if (msg.startsWith('!chatgpt')) {
-  const prompt = msg.replace('!chatgpt ', '');
+...
+
+if (isCommand(msg, "help")) {
+  api.sendMessage(help(), sender);
+}
+
+if (isCommand(msg, "roast")) {
+  api.sendMessage(commands.roast(), sender);
+}
+
+if (isCommand(msg, "inbox")) {
+  api.sendMessage(commands.inbox(msg), sender);
+}
+
+if (isCommand(msg, "chatgpt")) {
+  const prompt = msg.replace(getPrefix() + "chatgpt ", "");
   chatgpt(prompt).then(reply => api.sendMessage(reply, sender));
 }
 
-if (msg.startsWith('!gemini')) {
-  const prompt = msg.replace('!gemini ', '');
+if (isCommand(msg, "gemini")) {
+  const prompt = msg.replace(getPrefix() + "gemini ", "");
   gemini(prompt).then(reply => api.sendMessage(reply, sender));
 }
 
-if (msg.startsWith('!queen')) {
-  const prompt = msg.replace('!queen ', '');
+if (isCommand(msg, "queen")) {
+  const prompt = msg.replace(getPrefix() + "queen ", "");
   queenchat(prompt).then(reply => api.sendMessage(reply, sender));
 }
